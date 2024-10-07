b249c5d20293989d02a61c36c86a93186a447270
evelyncrystine <163007707+evelyncrystine@users.noreply.github.com>
2024-10-07 11:33:43 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    "Programação 2.0",
+    "O que é phyton?",
+    "O phyton é uma linguagem de programação"
+)
+
+criaCartao(
+    "One Piece",
+    "Em que ano One Piece foi criado?",
+    "One Piece foi criado no ano de 1997"
+)
+
+criaCartao(
+    "Biblía",
+    "Qual foi a décima e última praga do Egito?",
+    "A morte de todos os primogênitos do Egito, tanto humanos como animais"
+)
+
+criaCartao(
+    "Geografia",
+    "Quantos estados o Brasil tem ao todo?",
+    "O Brasil tem 26 estados ao todo"
+)
+
+criaCartao(
+    "Futebol",
+    "Em que data ocorreu a estreia oficial do Sport Club Corinthians Paulista?",
+    "10 de setembro de 1910, 10 dias após a sua fundação"
+)
+
+criaCartao(
+    "Naruto",
+    "Quem foi o responsável pelo massacre do clã Uchiha?",
+    "Itachi Uchiha foi o assassino, mas o real mandante desse massacre foi Danzo Shimura"
+)
+
